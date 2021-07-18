4e9bfab9e324a7910d340d57e1b2586ffddd22fb
Brad <brt.jst@gmail.com>
2021-07-17 18:26:23 -0700

    debug server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 // set up the express app
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 
 // middleware
@@ -19,9 +19,11 @@ app.use(require("./controllers"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 mongoose.connection.once("open", ()=>{
     app.listen(PORT, ()=> {
-        console.log ("Server: 8080")
+        console.log ("Now running on port 5000")
     })
 }) 
+
+// deployed app running, local host not firing.
 
 
 
